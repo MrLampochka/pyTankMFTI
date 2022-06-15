de8cb8e592ea28d704716b516ebc1ae3020e7f81
Valentine <khakvm@mail.com>
2022-06-15 17:54:29 +0300

    Test2

--- turt.py
+++ turt.py
@@ -1,5 +1,11 @@
-import turtle
+#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
 import random
+import turtle
 
 turtle.pensize(3)
 for i in range(0, 8):
